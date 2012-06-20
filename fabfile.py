df52124984218ee2118e7a91aa62d74dffeebea4
Danny Chen <mr.danny.chen@gmail.com>
2012-06-20 18:02:48 +1000

    Change fabfile to setup 2 vagrant web instance and 1 vagrant db instance

--- fabfile.py
+++ fabfile.py
@@ -2,17 +2,12 @@ from __future__ import with_statement
 from fabric.api import run, sudo, put, env, require, local, settings, prefix, cd
 from fabric.contrib.console import confirm
 import os
-import subprocess
 
 
 env.sites_dir           = "/srv/sites"
 env.virtualenv_dir      = "/virtualenvs"
 env.git_repo            = "git://github.com/munchie/django-example-project.git"
 env.virtualenv          = "webapp"
-env.nginx_conf          = "nginx_webapp.conf"
-env.nginx               = "nginx_webapp"
-env.gunicorn_conf       = "gunicorn_webapp.conf"
-env.gunicorn            = "gunicorn_webapp"
 env.project_name        = "myproject"
 env.code_dir            = os.path.join(env.sites_dir, env.virtualenv)
 env.project_dir         = os.path.join(env.code_dir, env.project_name)
@@ -22,7 +17,8 @@ env.bash_profile        = ".bash_profile"
 
 
 # These are the packages we need to install using aptitude install
-INSTALL_PACKAGES = [
+INSTALL_PACKAGES_WEB = [
+    "vim",
     "ntp",
     "git",
     "python2.7-dev",
@@ -32,9 +28,16 @@ INSTALL_PACKAGES = [
     "mercurial",
     "nginx",
     "libevent-dev",
-    "postgresql",
     "postgresql-server-dev-9.1",
     "libpq-dev"
+]
+
+
+INSTALL_PACKAGES_DB = [
+    "vim",
+    "ntp",
+    "git",
+    "postgresql"
 ]
 
 
@@ -57,61 +60,90 @@ def staging():
     env.branch          = "master"
 
 
-def vagrant():
-    """Setup local vagrant settings"""
-    raw_ssh_config      = subprocess.Popen(["vagrant", "ssh-config"], stdout=subprocess.PIPE).communicate()[0]
-    ssh_config          = dict([l.strip().split() for l in raw_ssh_config.split("\n") if l])
-    env.hosts           = ["127.0.0.1:%s" % ssh_config["Port"]]
-    env.user            = ssh_config["User"]
-    env.key_filename    = ssh_config["IdentityFile"]
+def vagrant_web():
+    """Setup local vagrant web server settings"""
+    env.hosts           = ["vagrant-web1", "vagrant-web2"]
+    env.user            = "vagrant"
+    env.key_filename    = "~/.vagrant.d/insecure_private_key"
     env.django_settings = "settings.development"
     env.branch          = "master"
     env.bash_profile    = ".vagrant_profile"
+    env.package_list    = INSTALL_PACKAGES_WEB
+
+
+def vagrant_db():
+    """Setup local vagrant db server settings"""
+    env.hosts        = ["vagrant-db1"]
+    env.user         = "vagrant"
+    env.key_filename = "~/.vagrant.d/insecure_private_key"
+    env.package_list = INSTALL_PACKAGES_DB
 
 
 # Provision instance
-def setup_vagrant():
-    """Bootstraps the Vagrant environment"""
-    require('hosts', provided_by=[vagrant])
-    stop_processes()
+def setup_vagrant_web():
+    """Bootstraps the vagrant web environment"""
+    require('hosts', provided_by=[vagrant_web])
+    stop_web_processes()
     install_packages()
     make_virtualenv()
     setup_sites_dir()
-    setup_db()
     clone_repo()
     pull()
     requirements()
+    syncdb()
     migrate()
     collectstatic()
+    setup_nginx()
+    setup_gunicorn()
     gunicorn_config()
     nginx_config()
-    start_processes()
+    start_web_processes()
+
+
+def setup_vagrant_db():
+    """Bootstraps the vagrant db environment"""
+    require('hosts', provided_by=[vagrant_db])
+    stop_db_process()
+    install_packages()
+    setup_sites_dir()
+    clone_repo()
+    setup_db()
+    db_config()
+    start_db_process()
+    create_db_and_user()
 
 
 # Sub commands
-def start_processes():
+def start_web_processes():
     """Starts nginx and gunicorn"""
     sudo("service nginx start")
-    sudo("start %(gunicorn)s" % env)
+    sudo("service gunicorn start")
 
 
-def stop_processes():
+def stop_web_processes():
     """Stops nginx and gunicorn"""
     with settings(warn_only=True):
         sudo("service nginx stop")
-        sudo("stop %(gunicorn)s" % env)
+        sudo("service gunicorn stop")
+
+
+def start_db_process():
+    """Starts postgresql"""
+    sudo("service postgresql start")
+
+
+def start_db_process():
+    """Stops postgresql"""
+    with settings(warn_only=True):
+        sudo("service postgresql stop")
 
 
 def install_packages():
     """Install required packages"""
     sudo("aptitude update")
-    sudo("aptitude -y install %s" % " ".join(INSTALL_PACKAGES))
+    sudo("aptitude -y install %s" % " ".join(env.package_list))
     sudo("easy_install pip")
     sudo("pip install virtualenv virtualenvwrapper")
-    # remove default nginx configuration
-    sudo("rm -f /etc/nginx/sites-enabled/default")
-    # create gunicorn log directory
-    sudo("mkdir -p /var/log/gunicorn")
 
 
 def make_virtualenv():
@@ -150,12 +182,54 @@ def setup_sites_dir():
 
 def setup_db():
     """Create the postgres db and db user for the app"""
+    with cd("/etc/postgresql/9.1/main"):
+        # remove the default config files
+        sudo("rm -f postgresql.conf pg_hba.conf")
+
+
+def db_config():
+    """Updates the postgresql config files"""
+    with cd("/etc/postgresql/9.1/main"):
+        sudo("rm -f postgresql.conf pg_hba.conf")
+        sudo("ln -sf %(code_dir)s/config/postgresql/postgresql.conf ." % env)
+        sudo("ln -sf %(code_dir)s/config/postgresql/pg_hba.conf ." % env)
+
+
+def create_db_and_user():
+    """Creates the db and user"""
     # warn if db user or db already exists
     with settings(warn_only=True):
-        # create db user (not a superuser, can create db, can't create roles, requires password)
-        sudo("createuser %(db_user)s -S -d -R -P" % env, user="postgres")
+        # create db user (not a superuser, can create db, can't create roles, no password)
+        sudo("createuser %(db_user)s -S -d -R" % env, user="postgres")
         # create db owned by db user
         sudo("createdb %(db_name)s -O %(db_user)s" % env, user="postgres")
+
+
+def setup_gunicorn():
+    """Setup after initial install of gunicorn"""
+    # create gunicorn log directory
+    sudo("mkdir -p /var/log/gunicorn")
+
+
+def gunicorn_config():
+    """Updates the gunicorn upstart config file"""
+    with cd("/etc/init"):
+        sudo("rm -f gunicorn.conf")
+        sudo("ln -sf %(code_dir)s/config/gunicorn/gunicorn.conf ." % env)
+
+
+def setup_nginx():
+    """Setup after initial install of nginx"""
+    with cd("/etc/nginx"):
+        # remove all the basic installed stuff
+        sudo("rm -rf conf.d/ fastcgi_params koi-* nginx.conf sites-* win-utf")
+
+
+def nginx_config():
+    """Updates the nginx config file"""
+    with cd("/etc/nginx"):
+        sudo("rm -f nginx.conf")
+        sudo("ln -sf %(code_dir)s/config/nginx/nginx.conf ." % env)
 
 
 def clone_repo():
@@ -205,29 +279,18 @@ def collectstatic():
             run('django-admin.py collectstatic -l')
 
 
-def gunicorn_config():
-    """Updates the gunicorn upstart config file"""
-    sudo("cp -f %(code_dir)s/config/%(gunicorn_conf)s /etc/init/%(gunicorn_conf)s" % env)
-
-
 def restart_gunicorn():
     """Restarts gunicorn"""
     with settings(warn_only=True):
         # Note: Upstart reload does not pick up changes to the conf file.
-        sudo("stop %(gunicorn)s" % env)
-        sudo("start %(gunicorn)s" % env)
-
-
-def nginx_config():
-    """Updates the nginx config file"""
-    sudo("cp -f %(code_dir)s/config/%(nginx_conf)s /etc/nginx/sites-available/%(nginx_conf)s" % env)
-    sudo("ln -sf /etc/nginx/sites-available/%(nginx_conf)s /etc/nginx/sites-enabled" % env)
+        sudo("service gunicorn stop")
+        sudo("service gunicorn start")
 
 
 def reload_nginx():
     """Reloads nginx"""
     with settings(warn_only=True):
-        sudo("nginx -s reload")
+        sudo("service nginx reload")
 
 
 def deploy():
@@ -236,8 +299,6 @@ def deploy():
     requirements()
     migrate()
     collectstatic()
-    gunicorn_config()
-    nginx_config()
     restart_gunicorn()
     reload_nginx()
 
