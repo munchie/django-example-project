54e9c6f5a616c71a7a834ed3dc9f139e698cf71c
Danny Chen <mr.danny.chen@gmail.com>
2012-06-20 17:57:03 +1000

    Change database settings for production

--- myproject/settings/production.py
+++ myproject/settings/production.py
@@ -9,8 +9,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'webapp',                      # Or path to database file if using sqlite3.
         'USER': 'webapp',                      # Not used with sqlite3.
-        'PASSWORD': 'w3bapp',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '10.10.10.20',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
