1d8e56759fbe6f345173fc25540fdb4b4032871a
Danny Chen <mr.danny.chen@gmail.com>
2012-06-20 19:38:41 +1000

    Run gunicorn in daemon mode

--- config/gunicorn/config.py
+++ config/gunicorn/config.py
@@ -3,6 +3,7 @@
 import multiprocessing
 
 bind            = "0.0.0.0:8000"
+daemon          = True
 workers         = multiprocessing.cpu_count() * 2 + 1
 worker_class    = "gevent"
 django_settings = "settings.production"
